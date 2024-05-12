0aca178ba9ed8bc856dd750fc6e7fbc294f80e06
chandradinata100 <113411375+chandradinata100@users.noreply.github.com>
2024-05-12 10:35:44 +0700

    Add files via upload

---
+++ tugas5/example-app/resources/views/biodata.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biodata Diri</title>
+    <body>
+    <div class="container">
+        <h1>Biodata Diri</h1>
+        <div class="detail">
+            <label for="npm">NPM:</label>
+            <span id="npm">222310008</span>
+        </div>
+        <div class="detail">
+            <label for="nama">Nama:</label>
+            <span id="nama">Chandradinata</span>
+        </div>
+        <div class="detail">
+            <label for="umur">Umur:</label>
+            <span id="umur">20 tahun</span>
+        </div>
+        <div class="detail">
+            <label for="alamat">Alamat:</label>
+            <span id="alamat">Jl. Kabandungan 1</span>
+        </div>
+        <div class="detail">
+            <label for="pekerjaan">Pendidikan:</label>
+            <span id="pekerjaan">Institut Bisnis Dan Informatika Kesatuan</span>
+        </div>
+        <div class="detail">
+            <label for="email">Email:</label>
+            <span id="email">222310008@student.ibik.ac.id</span>
+        </div>
+        <div class="detail">
+            <label for="telepon">No. Telepon:</label>
+            <span id="telepon">088291045255</span>
+        </div>
+        <div class="detail">
+            <label for="jk">Jenis Kelamin:</label>
+            <span id="jk">Pria</span>
+        </div>
+        <div class="detail">
+            <label for="t">Tinggi:</label>
+            <span id="t">165 cm</span>
+        </div>
+        <div class="detail">
+            <label for="b">Berat:</label>
+            <span id="b">65 kg</span>
+        </div>
+    </div>
+</body>
+    <style>
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            background-image: url('https://img.freepik.com/free-vector/memphis-blue-background-with-halftone-line-elements_1017-33622.jpg');
+            background-size: cover;
+            background-repeat: no-repeat;
+            color: #333;
+            padding: 20px;
+        }
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 15px rgba(0,0,0,0.1);
+        }
+        h1 {
+            text-align: center;
+            color: #007bff;
+        }
+        .detail {
+            margin-bottom: 15px;
+        }
+        .detail label {
+            font-weight: bold;
+            color: #555;
+        }
+        .detail span {
+            color: #777;
+        }
+    </style>
+</head>
+
+</html>
